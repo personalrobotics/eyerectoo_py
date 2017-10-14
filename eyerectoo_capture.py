b203361d3568e197aeca09996e22530f5ed410a0
Sherdil Niyaz <sniyaz@berkeley.edu>
2017-10-14 18:27:01 -0400

    Very basic. Need to get to the point where a video frame can be read in.

---
+++ eyerectoo_capture.py
@@ -0,0 +1,25 @@
+import socket
+
+#EyeRecTpp defaults.
+IP_ADRESS = "255.255.255.255"
+PORT = 2002
+ADDR = (IP_ADRESS, PORT)
+
+class EyeRecTooCapture():
+
+    def __init__(self, log=None):
+        # setting UDP socket.
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.socket.bind(ADDR)
+        self.logger = log
+
+
+    def read(self):
+        data, addr = self.socket.recvfrom(8192)
+        data = data
+        return data.split()
+
+
+test_capture = EyeRecTooCapture()
+        
