8173c18028859ec693a932ac33d2258c7a5c76ab
Sherdil Niyaz <sniyaz@berkeley.edu>
2017-10-18 17:21:51 -0400

    Added other gaze data to ROS message.

--- src/eyerectoo_publisher.py
+++ src/eyerectoo_publisher.py
@@ -5,6 +5,8 @@ import rospy
 from eyerectoo_stream.msg import Journal
 from eyerectoo_capture import EyeRecTooCapture
 
+import pdb
+
 def parse_journal_data(journal_data):
     ''' Parse EyeRecToo Journal into a Journal message.'''  
     outmsg = Journal()
@@ -15,7 +17,37 @@ def parse_journal_data(journal_data):
     # Split off the "J"
     outmsg.sync_timestamp =  int(journal_data[0][1:])
 
-    # TODO: Rest of journal fields once we have sync_timestap working.
+    outmsg.gaze_x = float(journal_data[2])
+    outmsg.gaze_y = float(journal_data[3])
+    outmsg.valid_gaze = int(journal_data[5]) == 1
+
+    outmsg.field_width = int(journal_data[11])
+    outmsg.field_height = int(journal_data[12])
+
+    aruco_markers_present = len(journal_data) > 30
+    outmsg.aruco_markers_present = aruco_markers_present
+    if aruco_markers_present:
+        outmsg.aruco_markers = journal_data[13]
+        # Remove the aruco marker data to make indicies consistent
+        journal_data = journal_data[:13] + journal_data[14:]
+        #pdb.set_trace()
+
+    
+    outmsg.left_pupil_x = float(journal_data[15])
+    outmsg.left_pupil_y = float(journal_data[16])
+    outmsg.left_pupil_width = float(journal_data[17])
+    outmsg.left_pupil_height = float(journal_data[18])
+    outmsg.left_pupil_angle = float(journal_data[19])
+    outmsg.left_pupil_valid = int(journal_data[20]) == 1
+
+    outmsg.right_pupil_x = float(journal_data[23])
+    outmsg.right_pupil_y = float(journal_data[24])
+    outmsg.right_pupil_width = float(journal_data[25])
+    outmsg.right_pupil_height = float(journal_data[26])
+    outmsg.right_pupil_angle = float(journal_data[27])
+    outmsg.right_pupil_valid = int(journal_data[28]) == 1
+
+    #pdb.set_trace()
 
     return outmsg
 
