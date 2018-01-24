522f74271f3d7b8edb718b534441bc96e3be70d4
Sherdil Niyaz <sniyaz@berkeley.edu>
2018-01-24 16:05:39 -0500

    [UNTESTED] Update CSV data streamer to fill in Aruco data in data vector.

--- src/csv_data_stream.py
+++ src/csv_data_stream.py
@@ -1,6 +1,7 @@
 import pandas
-import pdb
 from gaze_data_vector import GazeDataVector
+
+import pdb
 
 class CSVDataStream():
 
@@ -23,7 +24,23 @@ class CSVDataStream():
         new_data_vector.field_width = self.csv_data["field_width"][self.row_index]
         new_data_vector.field_height = self.csv_data["field_height"][self.row_index]
 
-        # TODO: Aruco stuff. Haha.
+        new_data_vector.aruco_markers_present = self.csv_data["aruco_markers_present"][self.row_index]
+        new_data_vector.aruco_IDs = []
+        new_data_vector.aruco_X_vals = []
+        new_data_vector.aruco_Y_vals = []
+
+        if new_data_vector.aruco_markers_present:
+            marker_id_string = self.csv_data["aruco_IDs"][self.row_index]
+            for string_id in marker_id_string.split(":"):
+                new_data_vector.aruco_IDs.append(int(string_id))
+
+            marker_x_coord_string = self.csv_data["aruco_X_vals"][self.row_index]
+            for string_x_coord in marker_x_coord_string,split(":")
+                new_data_vector.aruco_X_vals.append(float(string_x_coord))
+
+            marker_y_coord_string = self.csv_data["aruco_Y_vals"][self.row_index]
+            for string_y_coord in marker_y_coord_string.split(":"):
+                new_data_vector.aruco_Y_vals.append(float(string_y_coord))
 
         new_data_vector.left_pupil_x = self.csv_data["left_pupil_x"][self.row_index]
         new_data_vector.left_pupil_y = self.csv_data["left_pupil_y"][self.row_index]
